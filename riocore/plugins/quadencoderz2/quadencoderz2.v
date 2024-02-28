e1ae867fd8af68446df8908e4b3944e74c3f84ab
jesse schoch <bob@bob.com>
2024-02-28 12:49:19 -0800

    adding regular FAW

--- riocore/plugins/quadencoderz2/quadencoderz2.v
+++ riocore/plugins/quadencoderz2/quadencoderz2.v
@@ -20,6 +20,7 @@ module quadencoderz2
     reg [2:0] quadA_delayed = 0;
     reg [2:0] quadB_delayed = 0;
     reg [2:0] quadZ_delayed = 0;
+    reg last_z = 0;
     always @(posedge clk) quadA_delayed <= {quadA_delayed[1:0], a};
     always @(posedge clk) quadB_delayed <= {quadB_delayed[1:0], b};
     always @(posedge clk) quadZ_delayed <= {quadZ_delayed[1:0], z};
@@ -32,12 +33,13 @@ module quadencoderz2
         idx <= z;
         raw_a <= a;
         raw_b <= b;
-        if(quadZ_delayed == 1 && count_direction && count_enable) begin
-            // TODO: add direction bit
+        if(quadZ_delayed == 1 && count_direction && !last_z) begin
+            // TODO: this doesn't wreally work
            revs <= revs +1; 
-        end else if(quadZ_delayed == 1 && !count_direction && count_enable) begin
+        end else if(quadZ_delayed == 1 && !count_direction && !last_z) begin
             revs <= revs - 1;
         end
+        last_z <= quadZ_delayed;
         if (indexenable == 1 && indexout == 1 && quadZ_delayed == 1) begin
             indexout <= 0;
             count <= 0;
